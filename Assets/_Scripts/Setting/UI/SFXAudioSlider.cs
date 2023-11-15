dde26cd172606f1d656d8c7147c24d6fae5178a7
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-11-15 09:00:44 +0700

    feat: music slider

---
+++ Assets/_Scripts/Setting/UI/SFXAudioSlider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.Setting.UI
+{
+    [RequireComponent(typeof(Slider))]
+    public class SFXAudioSlider : MonoBehaviour
+    {
+        private Slider _slider;
+        private void Awake()
+        {
+            
+            _slider = GetComponent<Slider>();
+            
+            _slider.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(float value)
+        {
+            AudioManager.Instance.ChangeSfxVolume(value);
+        }
+
+    }
+}
