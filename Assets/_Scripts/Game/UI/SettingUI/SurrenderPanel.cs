ecf5b3eda8b41030a60f251d71a5e51439503cde
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-01-10 13:03:50 +0700

    feat: dissconnect

---
+++ Assets/_Scripts/Game/UI/SettingUI/SurrenderPanel.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Netcode;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Scripts.Game.UI.SettingUI
+{
+    public class SurrenderPanel : MonoBehaviour
+    {
+        [SerializeField] private Button _surrenderButton;
+        [SerializeField] private Transform _surrenderConfirmPanel;
+        
+        
+        void Start()
+        {
+            _surrenderButton.onClick.AddListener(ReturnToHome);
+        }
+
+        
+        
+        private void ReturnToHome()
+        {
+            NetworkManager.Singleton.Shutdown();
+            
+            AssetSceneManager.LoadScene(AssetSceneManager.AssetScene.LobbyScene.ToString());
+            
+        }
+        
+        
+    }
+}
