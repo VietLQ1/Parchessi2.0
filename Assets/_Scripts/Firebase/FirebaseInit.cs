64363a66d91e2f3d266828dab7c199c4414b6779
VietLQ1 <vietdz145@gmail.com>
2024-01-13 00:01:34 +0700

    fix: resolve old firebase files error

---
+++ Assets/_Scripts/Firebase/FirebaseInit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+public class FirebaseInit : MonoBehaviour
+{
+    public UnityEvent OnFirebaseInitialized = new UnityEvent();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+       
+    }
+}
---
+++ Assets/_Scripts/Firebase/PlayerSaveManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUtilities;
+using System.Threading.Tasks;
+
+public class PlayerSaveManager : SingletonMonoBehaviour<PlayerSaveManager>
+{
+    private const string Player_Key = "PLAYER_KEY";
+   
+    private void Start()
+    {
+        
+    }
+    public void SavePlayer(PlayerData playerData)
+    {
+      
+    }
+    
+}
